2c9e32c58a1f8ca0c3ca848c06bd4b2263dfbfe6
zigen <zigen@horol.org>
2020-07-03 22:13:06 +0900

    initialize project

---
+++ Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.EntityFrameworkCore.Storage.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public Room Room { get; set; }
+        [Timestamp]
+        public byte[] TimeStamp { get; set; }
+
+    }
+}
---
+++ Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+    }
+}
